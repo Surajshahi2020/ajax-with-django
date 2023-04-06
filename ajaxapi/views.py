7618a8203b76764181dc913532d3b15fec6bca20
Suraj shahi <80339587+Surajshahi2020@users.noreply.github.com>
2023-04-06 19:13:57 +0545

    Update views.py
    get, put and delete

--- ajaxapi/views.py
+++ ajaxapi/views.py
@@ -24,4 +24,47 @@ class MemberCreateAPiView(APIView):
                 "success":"Ok"
             }
             return JsonResponse(message, status=200)
+        
+class MemberGetAPiView(APIView):
+    def get(self,request):
+        member_data = []
+        memdata = Member.objects.all()
+        for data in memdata:
+            member_data.append({
+                    "id": data.id,
+                    "name": data.name,
+                    "email": data.email,
+                    "password": data.password
+                })
+        message={
+            "success":"Ok",
+            "data":member_data
+        }
+        return JsonResponse(message, status=200) 
+    
+class MemberPutAPiView(APIView):
+    def put(self,request,pk):
+        data = json.loads(request.body)
+        edit = Member.objects.get(id=pk)
+        edit.name=data["name"]
+        edit.email=data["email"]
+        edit.password=data["password"]
+        edit.save()
+        message={"success":"Ok",
+                "data": {
+                        "id": edit.id,
+                        "name":edit.name,
+                        "email": edit.email,
+                        "password":edit.password
+            }
+        }
+        return JsonResponse(message, status=200)  
+    
+class MemberDeleteAPiView(APIView):
+    def delete(self,request,pk):
+        print(request.data)
+        edit = Member.objects.get(id=pk)
+        edit.delete()
+        message={ "success":"Ok"}
+        return JsonResponse(message, status=200)        
 
